88ec521d4f3a9718f4c180fa30a641b816770e9b
Xav-Sathorn <x.coenen.dev@gmail.com>
2022-06-30 19:19:43 +0200

    radio ok

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,8 @@ import Cards from "./Cards";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
   /* le useEffect se joue lorsque le component est monté */
   useEffect(() => {
     axios
@@ -22,11 +24,33 @@ const Countries = () => {
           defaultValue={rangeValue}
           onChange={(e) => setRangeValue(e.target.value)}
         />
-      </ul>
-      <ul>
-        {data.slice(0, rangeValue).map((country, index) => (
-          <Cards key={index} country={country} />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
         ))}
+      </ul>
+      {/* si selelectedRadio is true alors tu annnules la recherche et affiche tous les pays */}
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Cards key={index} country={country} />
+          ))}
       </ul>
     </div>
   );
