5c29e6f51570083f5afcfa59ccde2faa5f248300
Xav-Sathorn <x.coenen.dev@gmail.com>
2022-06-30 09:12:50 +0200

    first git

---
+++ src/pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>à Propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit harum
+        esse quae rerum, reiciendis non natus corrupti ex omnis culpa quod
+        facere, tenetur est! Eveniet placeat mollitia officia nostrum libero
+        earum enim ab iste suscipit fugit aperiam autem consequatur minima vero
+        corporis, voluptates cumque soluta! Placeat aliquid quibusdam distinctio
+        rem sit. Quo non molestiae magnam, reiciendis consequuntur sunt ratione
+        quod, blanditiis nulla laudantium dolore nemo ipsa optio debitis minus
+        error cupiditate amet maxime! Nihil, vero aspernatur? Sunt culpa unde
+        ipsum voluptate earum ad veniam, corrupti aut quas vitae consectetur
+        optio omnis, at tempora sint tenetur asperiores quo autem sit ab.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam nam
+        soluta doloremque est perferendis. Minima sit perspiciatis adipisci
+        eveniet excepturi maiores, temporibus maxime voluptate sed. Dicta odio
+        sed perspiciatis ipsum reprehenderit eum at porro exercitationem quod.
+        Iure dicta tenetur reprehenderit aperiam vitae corporis tempore soluta
+        ea sit ab dolorum necessitatibus consequatur, minus magni in voluptate.
+        Delectus nulla eos vitae a officia ex animi perferendis perspiciatis
+        alias omnis? At et earum iure itaque alias debitis, odio laudantium non
+        quos, autem eos facilis aliquam ipsam. Illum doloremque ab aut
+        recusandae nemo vitae quis eum adipisci rerum dolore iste incidunt
+        soluta et reprehenderit perspiciatis ipsam cumque excepturi debitis
+        deserunt voluptatum, veniam velit veritatis nostrum! Perferendis
+        perspiciatis, iure harum soluta eius corporis, odit fuga voluptates amet
+        eaque voluptate rem, hic explicabo repellat voluptas. Minima sunt quo
+        vitae cumque, et consectetur libero perferendis soluta! Obcaecati
+        similique facilis nihil, possimus dolorem perferendis suscipit, rerum
+        earum cupiditate vel eos repudiandae sint iusto! Aperiam ab tempora
+        mollitia nulla reprehenderit omnis id eius, blanditiis vero ipsam, magni
+        aspernatur quod eaque deleniti alias qui maiores. Aperiam accusamus
+        veniam consequuntur, fugit harum earum ut laborum velit quod id sit
+        perspiciatis, reprehenderit nihil ipsa culpa. Recusandae praesentium
+        corporis eius cum voluptatibus dolores.
+      </p>
+    </div>
+  );
+};
+
+export default About;
