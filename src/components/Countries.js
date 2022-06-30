16044f94cbcae02e164815c69c45b565d5b16428
Xav-Sathorn <x.coenen.dev@gmail.com>
2022-06-30 09:12:30 +0200

    first push

---
+++ src/components/Countries.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Cards from "./Cards";
+
+const Countries = () => {
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  /* le useEffect se joue lorsque le component est monté */
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <div className="countries">
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+      </ul>
+      <ul>
+        {data.slice(0, rangeValue).map((country, index) => (
+          <Cards key={index} country={country} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
